d187d8d74ff1e5cf1840c7ac8704324fcc551eba
Igor_Boldysh <bushemi2001@gmail.com>
2019-09-29 11:41:10 +0300

    was added Login filter for requests to log in.

---
+++ src/main/java/com/bushemi/web/filters/LoginFilter.java
@@ -0,0 +1,53 @@
+package com.bushemi.web.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger("LoginFilter");
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(true);
+        String url = httpRequest.getRequestURL().toString();
+        LOG.info("url = {}. Session id = {}", url, session.getId());
+        if (url.endsWith("users")) {
+            String textFromRequest = textFromRequest(httpRequest);
+            if (!textFromRequest.contains("isNewUser=true")) {
+
+                session.setAttribute("textFromRequest", textFromRequest);
+                httpResponse.sendRedirect("/authentication");
+            }
+        }
+        chain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private String textFromRequest(HttpServletRequest req) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = req.getReader()) {
+            reader.lines().forEach(builder::append);
+        }
+        LOG.info("result of textFromRequest method = {}", builder);
+        return builder.toString();
+    }
+}
