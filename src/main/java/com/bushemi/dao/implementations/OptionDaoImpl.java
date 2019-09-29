6d5e8a0001a566b42280e3670f723675e8906d2b
Igor_Boldysh <bushemi2001@gmail.com>
2019-09-29 19:52:07 +0300

    was added method to find all options by question id.

--- src/main/java/com/bushemi/dao/implementations/OptionDaoImpl.java
+++ src/main/java/com/bushemi/dao/implementations/OptionDaoImpl.java
@@ -2,19 +2,25 @@ package com.bushemi.dao.implementations;
 
 import com.bushemi.dao.entity.Option;
 import com.bushemi.dao.interfaces.OptionDao;
+import com.bushemi.exceptions.DbException;
 import com.bushemi.service.interfaces.DbConnectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OptionDaoImpl extends AbstractDao<Option> implements OptionDao {
     private static final Logger LOG = LoggerFactory.getLogger("OptionDaoImpl");
+    private DbConnectionService dbConnector;
 
     public OptionDaoImpl(DbConnectionService dbConnector) {
         super(dbConnector, LOG);
+        this.dbConnector = dbConnector;
     }
 
     @Override
@@ -64,5 +70,29 @@ public class OptionDaoImpl extends AbstractDao<Option> implements OptionDao {
         entity.setCorrect(resultSet.getBoolean(4));
 
         return entity;
+    }
+
+    @Override
+    public List<Option> finAllByQuestionId(Long questionId) {
+        List<Option> entities = new ArrayList<>();
+        Connection connection = dbConnector.getConnection();
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT id, main_text, question_id, is_correct FROM options WHERE question_id = ?;")) {
+
+            preparedStatement.setLong(1, questionId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Option entity = getEntityFromResultSet(resultSet);
+                entities.add(entity);
+            }
+        } catch (SQLException e) {
+            LOG.error("Can't find options by question id = {}. {}", questionId, e.getMessage());
+            throw new DbException(e);
+        } finally {
+            dbConnector.releaseConnection(connection);
+        }
+
+        return entities;
     }
 }
--- src/main/java/com/bushemi/dao/interfaces/OptionDao.java
+++ src/main/java/com/bushemi/dao/interfaces/OptionDao.java
@@ -2,5 +2,8 @@ package com.bushemi.dao.interfaces;
 
 import com.bushemi.dao.entity.Option;
 
+import java.util.List;
+
 public interface OptionDao extends CrudOperationsInterface<Option> {
+    List<Option> finAllByQuestionId(Long questionId);
 }
